5a380719a76b3f8d93ae5bdba8c6641b98620002
PieterNatanael <pieternatanael@icloud.com>
2023-10-27 20:05:28 +0800

    Initial Commit

---
+++ Tormentorr/TormentorrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TormentorrApp.swift
+//  Tormentorr
+//
+//  Created by Pieter Yoshua Natanael on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TormentorrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
